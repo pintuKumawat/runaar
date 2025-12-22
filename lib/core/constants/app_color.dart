247c6c6a17fe5f422e529c42168793e225acb725
vkhatri148 <vkhatri1408@gmail.com>
2025-12-22 17:28:12 +0530

    Profile Pages

--- lib/core/constants/app_color.dart
+++ lib/core/constants/app_color.dart
@@ -3,12 +3,14 @@ import 'package:flutter/material.dart';
 class AppColor {
   // Color mainColor = const Color(0xFF2E5349);
   Color mainColor = const Color(0xFF000000);
-  Color secondColor = const Color.fromRGBO(33, 150, 243, 1);
-  Color textColor = Colors.black;
+  Color secondColor = const Color(0xFF2196F3);
+  Color textColor = const Color(0XFF000000);
   // Color themeColor = Color(0xFFB4CCC6);
-  Color themeColor = Colors.grey;
-  Color backgroundColor = Color(0xffF6F6F6);
-  Color buttonColor = Colors.white;
+  Color themeColor = const Color(0xFF9E9E9E);
+  Color backgroundColor = const Color(0xffF6F6F6);
+  Color buttonColor = const Color(0XFFFFFFFF);
+
+  Color iconColors = const Color(0XFF07869C);
 }
 
 final AppColor appColor = AppColor();
